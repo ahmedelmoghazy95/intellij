8f2f79f8fe249de7e6e64c64b936928b507c2578
layman <layman@sumerge.com>
2021-06-27 14:07:04 +0200

    InspectorDailyVisits Tests

--- src/main/java/com/sumerge/momra/pages/InspectorDailyVisitsPage.java
+++ src/main/java/com/sumerge/momra/pages/InspectorDailyVisitsPage.java
@@ -21,9 +21,60 @@ public class InspectorDailyVisitsPage extends BasePage {
     @FindBy(id = "visits-viewList-visitDate")
     private WebElement visitDateField;
 
+    @FindBy(id ="visits-viewList-siteName")
+    private WebElement visitSiteNameField;
+
+    @FindBy(id ="visits-viewList-licenseId")
+    private WebElement visitLicenseIdField;
+
+    @FindBy(id ="visits-viewList-licenseStatus")
+    private WebElement visitLicenseStatusField;
+
+
+    @FindBy(id ="visits-viewList-address")
+    private WebElement visitAddressField;
+
+    @FindBy(id ="visits-viewList-licenseOwnerName")
+    private WebElement visitLicenseOwnerNameField;
+
+    @FindBy(id ="visits-viewList-numberOfBeds")
+    private WebElement visitNumberOfBedsField;
+
+    @FindBy(id ="visits-viewList-licensePhone")
+    private WebElement visitLicensePhoneField;
+
+    @FindBy(id ="open-map")
+    private WebElement visitLocationButton;
+
+    @FindBy(id ="closeDialogButton")
+    private WebElement visitCloseLocationButton;
+
     public String getVisitNumberFieldValue(){
         return Utilities.waitAndGetWebElementText( visitNumberField, wait);
     }
-
+    public String getVisitDateFieldValue(){
+        return Utilities.waitAndGetWebElementText( visitDateField, wait);
+    }
+    public String getVisitSiteNameFieldValue(){
+        return Utilities.waitAndGetWebElementText( visitSiteNameField, wait);
+    }
+    public String getVisitLicenseIdFieldValue(){ return Utilities.waitAndGetWebElementText( visitLicenseIdField, wait);
+    }
+    public String getVisitLicenseStatusFieldValue(){
+        return Utilities.waitAndGetWebElementText( visitLicenseStatusField, wait);
+    }
+    public String getVisitAddressFieldValue(){ return Utilities.waitAndGetWebElementText( visitAddressField, wait);}
+    public String getVisitLicenseOwnerNameFieldValue(){
+        return Utilities.waitAndGetWebElementText( visitLicenseOwnerNameField, wait);
+    }
+    public String getVisitNumberOfBedsFieldValue(){
+        return Utilities.waitAndGetWebElementText( visitNumberOfBedsField, wait);
+    }
+    public String getVisitLicensePhoneFieldValue(){
+        return Utilities.waitAndGetWebElementText( visitLicensePhoneField, wait);
+    }
+    public void clickOnVisitLocationButton() {Utilities.waitAndClickOnWebElement(visitLocationButton, wait, driver);
+    }
+    public void clickOnVisitCloseLocationButton() {Utilities.waitAndClickOnWebElement(visitCloseLocationButton, wait, driver); }
 
 }
