eff0eb0ff30caff5a6b709c834bff00498aa7d1b
ahmed elmoghazy <amoghazy@sumerge.com>
2021-09-12 03:33:37 +0200

    initial commit

---
+++ src/test/java/APItests.java
@@ -0,0 +1,30 @@
+import static io.restassured.RestAssured.*;
+import io.restassured.response.Response;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class APItests {
+    @Test
+    void test1 (){
+
+       Response response = get("https://reqres.in/api/users?page=2");
+
+        System.out.println("Response : " + response.asString());
+        System.out.println("status code : "+ response.getStatusCode());
+        System.out.println("body : "+ response.getBody().asString());
+        System.out.println("time taken : "+ response.getTime());
+        System.out.println("header : "+ response.getHeader("content-type"));
+
+        int statuscode = response.statusCode();
+        Assert.assertEquals(statuscode,200);
+
+    }
+
+    @Test
+    void test2(){
+        given().
+                get("https://reqres.in/api/users?page=2").
+                then().
+                statusCode(200);
+    }
+}
